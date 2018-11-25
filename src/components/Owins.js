881321568f55e8e9406c10a1a40a8695b8c7fe86
bkfwebdev <bryant.feld@gmail.com>
2018-11-25 14:36:14 -0500

    updating project structure and adding components

---
+++ src/components/Owins.js
@@ -0,0 +1,12 @@
+import React,{ Component } from "react";
+
+class Owins extends Component {
+    render (){
+        <div className = "screen-win-one" >
+            <header><h1>Tic Tac Toe</h1><p class="message">Winner</p><a href="#board" class="button">New game</a></header>
+        </div>
+    }
+
+}
+
+export default Owins
---
+++ src/components/Xwins.js
@@ -0,0 +1,12 @@
+import React,{ Component } from "react";
+
+class Xwins extends Component {
+    render (){
+        <div className = "screen-win-two " >
+            <header><h1>Tic Tac Toe</h1><p class="message">Winner</p><a href="#board" className="button">New game</a></header>
+        </div>
+    }
+
+}
+
+export default Xwins;
